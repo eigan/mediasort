7a8fe2e038e44b68872088070ad99de51b469f5e
Einar Gangsø <mail@einargangso.no>
2018-04-29 20:07:18 +0200

    Set correct version

--- src/Application.php
+++ src/Application.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputOption;
 
 class Application extends SymfonyApplication
 {
-    const VERSION = '0.12';
+    const VERSION = '0.12.2';
 
     /**
      * @var string
